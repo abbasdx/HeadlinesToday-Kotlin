59fe07922fcfe5d13bee4a5fc78871ccaee1bd4b
Abbas Ansari <iamabbas.self@gmail.com>
2025-02-10 23:14:53 +0530

    Update HomeScreenUI.kt

--- app/src/main/java/com/example/newsapp/presentation/screen/HomeScreenUI.kt
+++ app/src/main/java/com/example/newsapp/presentation/screen/HomeScreenUI.kt
@@ -10,7 +10,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -18,12 +20,14 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -44,8 +48,37 @@ fun HomeScreenUI(
     navController: NavController,
     viewModel: NewsAppViewModel
 ) {
-    val searchTerm = remember { mutableStateOf("") }
+    val scrollState = rememberLazyListState()
+    val selectedCategory = remember { mutableStateOf("") }
+    val searchTerm = rememberSaveable { mutableStateOf("") }
     val state = viewModel.state.collectAsState()
+    val newsCategories = remember {
+        mutableStateOf(
+            listOf(
+                "Business",
+                "Entertainment",
+                "General",
+                "Health",
+                "Science",
+                "Sports",
+                "Technology",
+                "World",
+                "Politics",
+                "Education",
+                "Lifestyle",
+                "Environment",
+                "Crime",
+                "Travel",
+                "Food",
+                "Finance",
+                "Automobile",
+                "Startups",
+                "Fashion",
+                "Gaming",
+                "Weather"
+            )
+        )
+    }
 
     if (state.value.loading == true) {
         Box(
@@ -61,6 +94,7 @@ fun HomeScreenUI(
             modifier = Modifier.fillMaxSize()
         ) {
             OutlinedTextField(value = searchTerm.value,
+                singleLine = true,
                 shape = RoundedCornerShape(15.dp),
                 label = { Text(text = "Search") },
                 placeholder = { Text(text = "Type, Tap, and Get the News...") },
@@ -75,8 +109,37 @@ fun HomeScreenUI(
                             })
                     )
                 },
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 9.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 9.dp)
             )
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(9.dp),
+                state = scrollState
+            ) {
+                items(newsCategories.value) {
+                    Card(
+                        colors = CardDefaults.cardColors(
+                            containerColor = if (it == selectedCategory.value) {
+                                MaterialTheme.colorScheme.primary
+                            } else {
+                                MaterialTheme.colorScheme.surfaceVariant
+                            }
+                        ),
+                        modifier = Modifier
+                            .padding(horizontal = 5.dp)
+                            .clickable {
+                                viewModel.getEverything(q = it)
+                                selectedCategory.value = it
+                            },
+                        shape = RoundedCornerShape(5.dp)
+                    ) {
+                        Text(it, modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp))
+                    }
+                }
+            }
             val data: ApiResponse? = state.value.data
             if (data?.articles!!.isEmpty()) {
                 Text(text = "No Data Found!")
@@ -91,7 +154,19 @@ fun HomeScreenUI(
                                 .padding(start = 9.dp, end = 9.dp, top = 6.dp, bottom = 6.dp),
                             elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
                             onClick = {
-                                navController.navigate(CategoryScreen)
+                                navController.navigate(
+                                    CategoryScreen(
+                                        author = article.author,
+                                        content = article.content,
+                                        description = article.description,
+                                        publishedAt = article.publishedAt,
+                                        id = article.source?.id,
+                                        name = article.source?.name,
+                                        title = article.title,
+                                        url = article.url,
+                                        urlToImage = article.urlToImage
+                                    )
+                                )
                             }
                         ) {
                             Column {
