47de67d4726c71e7ef6539ea38958cfd757e005b
Abbas Ansari <iamabbas.self@gmail.com>
2025-02-10 23:17:45 +0530

    Update AppNavigation.kt

--- app/src/main/java/com/example/newsapp/presentation/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/newsapp/presentation/navigation/AppNavigation.kt
@@ -5,6 +5,9 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.example.newsapp.data.model.Article
+import com.example.newsapp.data.model.Source
 import com.example.newsapp.presentation.NewsAppViewModel
 import com.example.newsapp.presentation.navigation.routes.CategoryScreen
 import com.example.newsapp.presentation.navigation.routes.HomeScreen
@@ -15,18 +18,33 @@ import com.example.newsapp.presentation.screen.SearchScreenUI
 
 
 @Composable
-fun AppNavigation(modifier: Modifier = Modifier,viewModel: NewsAppViewModel) {
+fun AppNavigation(modifier: Modifier = Modifier, viewModel: NewsAppViewModel) {
     val navController = rememberNavController()
-    NavHost(navController= navController, startDestination = HomeScreen){
-       composable<HomeScreen>{
-           HomeScreenUI(navController=navController,viewModel=viewModel)
-       }
-        composable<CategoryScreen>{
+    NavHost(navController = navController, startDestination = HomeScreen) {
+        composable<HomeScreen> {
+            HomeScreenUI(navController = navController, viewModel = viewModel)
+        }
+        composable<CategoryScreen> {
+            val categoryScreen = it.toRoute<CategoryScreen>()
+            val article = Article(
+                author = categoryScreen.author,
+                content = categoryScreen.content,
+                description = categoryScreen.description,
+                publishedAt = categoryScreen.publishedAt,
+                source = Source(
+                    id = categoryScreen.id,
+                    name = categoryScreen.name
+                ),
+                title = categoryScreen.title,
+                url = categoryScreen.url,
+                urlToImage = categoryScreen.urlToImage
+            )
             CategoryDetailUI(
+                article = article,
                 navController = navController
             )
         }
-        composable<SearchScreen>{
+        composable<SearchScreen> {
             SearchScreenUI(navController = navController, viewModel = viewModel)
 
         }
